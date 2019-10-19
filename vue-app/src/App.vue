669056ee3fcb58959aeb379fb4b0fce5e33a4f96
1-Sisyphe <21976395+1-Sisyphe@users.noreply.github.com>
2019-10-19 14:32:44 +0200

    initial commit

---
+++ vue-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="container">
+    <h1>Giffothèque de France</h1>
+
+    <gif-cards :gifs="gifs"/>
+  </div>
+</template>
+
+<script>
+ import GifCards from '@/components/GifCards.vue'
+ import gifs from '@/assets/reddit_data.json'
+export default {
+  name: 'app',
+  components: {
+    GifCards,
+  },
+  data() {
+    return {
+      gifs: gifs
+    }
+  },
+}
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
---
+++ vue-app/src/components/GifCards.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="gif-cards" class="col s12 cards-container">
+    <div class="col s12 m6 l4" v-for="gif in gifs" :key="gif.id">
+      <div class="card">
+        <div class="card-image">
+          <img :src="gif.preview" />
+        </div>
+        <div class="card-content">
+          <span class="card-title">{{gif.title}}</span>
+          <p>
+            <i class="material-icons" style="vertical-align: -6px;">arrow_upward</i>
+            {{gif.karma}}
+          </p>
+          <p>
+            <i class="material-icons" style="vertical-align: -6px;">calendar_today</i>
+            {{gif.date | moment("MM-DD-YY")}}
+          </p>
+          {{gif.author}}
+        </div>
+        <div class="card-action">
+          <a :href="gif.permalink">r/france</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gif-cards",
+  props: {
+    gifs: Array
+  }
+};
+</script>
+
+<style scoped>
+.cards-container {
+  column-break-inside: avoid;
+}
+.cards-container .card {
+  display: inline-block;
+  overflow: visible;
+}
+
+@media only screen and (max-width: 600px) {
+  .cards-container {
+    -webkit-column-count: 1;
+    -moz-column-count: 1;
+    column-count: 1;
+  }
+}
+@media only screen and (min-width: 601px) {
+  .cards-container {
+    -webkit-column-count: 2;
+    -moz-column-count: 2;
+    column-count: 2;
+  }
+}
+@media only screen and (min-width: 993px) {
+  .cards-container {
+    -webkit-column-count: 3;
+    -moz-column-count: 3;
+    column-count: 3;
+  }
+}
+.text-center {
+  text-align: center;
+}
+</style>
